032f68e8f791a2c1d2fe446042c15f93d0be5491
Rk-Dinesh <rdkumar881@gmail.com>
2026-03-27 21:36:48 +0530

    chore: Remove generated build artifacts and distribution files.

--- dist/assets/Schedule-DZHI1BtO.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CFWez2Sd.js";const t=()=>e.jsx("div",{children:"Schedule"});export{t as default};
--- dist/assets/romaa logo-Cph0YorF.js
+++
@@ -1,1 +0,0 @@
-const o="/assets/romaa%20logo-CBU5XiFP.png";export{o as L};
