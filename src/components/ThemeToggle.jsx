95ef73b2da69fa788ea42cc115d636945e24bea5
Rk-Dinesh <rdkumar881@gmail.com>
2026-03-29 12:43:14 +0530

    update ThemeToggle to import useTheme from ThemeContext instead of useTheme hook file

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,5 @@
 import { Sun } from "lucide-react";
-import { useTheme } from "../context/useTheme";
+import { useTheme } from "../context/ThemeContext";
 import { TbMoonStars } from "react-icons/tb";
 
 const ThemeToggle = () => {
