9cc6f1f0df3a4e3070c1c89d51c2899029f2bcf4
Rk-Dinesh <rdkumar881@gmail.com>
2026-04-03 11:32:17 +0530

    build: generate production build assets for frontend application

---
+++ dist/assets/index-DqzWb7fh.js
@@ -0,0 +1,1 @@
+import{br as r}from"./index-BjJtLzDZ.js";var a=r();export{a as r};
---
+++ dist/assets/index-legacy-DfBbYBLp.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-C1bmiAYt.js"],function(e,t){"use strict";var r;return{setters:[e=>{r=e.br}],execute:function(){e("r",r())}}});
